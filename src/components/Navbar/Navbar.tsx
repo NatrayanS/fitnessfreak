e4ecadff6372cc74a72d521226f836b2d9a03b4f
NatrayanS <s.natrayan06ayan96@gmail.com>
2024-03-04 22:48:31 +0530

    update

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '@/assets/logo.png'
+import {IoIosBody} from  "react-icons/io"
+import './Navbar.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Image src={logo} alt="Logo" />
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        <Link href='/profile'><IoIosBody /></Link>
+        <button>SignOut</button>
+    </nav>
+  )
+}
+
+export default Navbar
